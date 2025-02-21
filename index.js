851f2f881a5537209ee0c1a235a592df913568e6
Jonrie Morales Badiang <68489573+JonrieMain@users.noreply.github.com>
2025-02-21 13:09:36 +0800

    modify

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 let no = document.querySelector("#no");
+let sad1 = document.querySelector('.sad1');
 
+let imgNum = 0;
 
+const procc = ()=>{
+    let randY = Math.floor(Math.random() * 125);
+    let randX = Math.floor(Math.random() * 100);
+    no.style.position="fixed";
+    no.style.left=`${randX}%`;
+    no.style.top=`${randY}%`;
+
+    sad1.style.opacity="1";
+    if(imgNum === 3){
+        sad1.src=`./assets/sad3.gif`;
+    }else if(imgNum !== 3){
+        sad1.src=`./assets/sad${imgNum+1}.gif`;
+        imgNum++;
+    }
+}
 
 no.addEventListener("mouseover",()=>{
-    let randY = Math.floor(Math.random() * 125);
-    let randX = Math.floor(Math.random() * 100);
-    no.style.position="fixed";
-    no.style.left=`${randX}%`;
-    no.style.top=`${randY}%`;
-
-
+    procc();
 });
+
 no.addEventListener("click",()=>{
-    let randY = Math.floor(Math.random() * 125);
-    let randX = Math.floor(Math.random() * 100);
-    no.style.position="fixed";
-    no.style.left=`${randX}%`;
-    no.style.top=`${randY}%`;
-
-
+    procc();
 });
